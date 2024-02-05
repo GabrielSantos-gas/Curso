b44c885bf2166d65ac72525b3e7d77235673f50a
Gabriel <gabrielantoniio1997@gmail.com>
2024-02-04 23:56:27 -0300

    commit

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html') 
+    return render (request, 'recipes/pages/home.html', context={
+        'name': 'Gabriel Santos',
+        'recipes': [make_recipe() for _ in range(10)],
+
+    }) 
 
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-id.html')
+    return render (request, 'recipes/pages/recipe-id.html', context={
+        'name': 'Gabriel Santos',
+        'recipes': [make_recipe()],
+    })
 
 
 
