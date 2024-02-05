a2a577ea0688be3e11459b3e4986a0dc52c9d545
Gabriel <gabrielantoniio1997@gmail.com>
2024-02-05 19:37:48 -0300

    commit

--- recipes/views.py
+++ recipes/views.py
@@ -3,22 +3,18 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render (request, 'recipes/pages/home.html', context={
-        'name': 'Gabriel Santos',
-        'recipes': [make_recipe() for _ in range(10)],
+    return render (request, 'recipes/pages/home.html', 
+                   context={ 'recipes': [make_recipe() for _ in range(10)],
+                            'is_detail_page': False
 
     }) 
 
 
-
 def recipe(request, id):
-    return render (request, 'recipes/pages/recipe-id.html', context={
-        'name': 'Gabriel Santos',
-        'recipes': [make_recipe()],
-    })
-
-
-
+    recipe_data = make_recipe()  # Substitua isso pela lógica real para obter os dados da receita
+    return render(request, 'recipes/pages/recipe-id.html', 
+                  context={'recipe': recipe_data, 
+                           'is_detail_page': True})
 
 
 
